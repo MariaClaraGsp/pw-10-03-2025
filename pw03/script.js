29454660bd594fcc548ded78660143f0fbed9a51
MariaClaraGsp <maria2006gsp@gmail.com>
2025-03-10 10:05:56 -0300

    Add files via upload

--- pw03/script.js
+++ pw03/script.js
@@ -24,3 +24,22 @@ for (let i=0; i<paragrafo1.length; i++){
 
     console.log(paragrafo1[i].textContent);
 }
+
+//acesso ao conteúdo dos elementos html
+
+document.getElementById("paragrafo1").textContent= "texto alterado";
+
+//document.
+
+
+//eventos
+var botao1 = document.getElementById("botao1");
+botao1.onclick = function(){
+botao1.style.backgroundColor = "green";
+    alert("clicou!");
+}
+var botao2 = document.getElementById("botao2");
+var paragrafo2 = document.getElementById("paragrafo2");
+botao2.onclick = function(){
+paragrafo2.textContent = "texto alterado";
+}
